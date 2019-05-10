a371511926dacda8ea5d6e8b9d9c8772106fb26a
Panya Farnette <naienko@gmail.com>
2019-05-10 10:04:39 -0500

    Get one and Get all written NO queries

---
+++ BangazonAPI/Controllers/CustomerController.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public CustomerController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        [HttpGet]
+        //this function gets a List of all Customers in the database
+        // TODO: add 'include' queries
+        public async Task<IActionResult> Get(string include)
+        {
+            //create the SQL as a string, in order to be able to add to it with the 'include' queries
+            string sql = @"SELECT c.Id, c.FirstName, c.LastName 
+                                FROM Customer c";
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = sql;
+
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+                    List<Customer> customers = new List<Customer>();
+
+                    while (reader.Read())
+                    {
+                        Customer customer = new Customer
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName"))
+                        };
+
+                        customers.Add(customer);
+                    }
+                    reader.Close();
+
+                    return Ok(customers);
+                }
+            }
+        }
+
+        [HttpGet("{id}", Name = "GetCustomer")]
+        //this function gets a single Customer from the database, by id
+        // TODO: add 'include' queries
+        public async Task<IActionResult> Get([FromRoute] int id, string include)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // TODO: refactor SQL command to handle 'include' queries -- split into string as above
+                    cmd.CommandText = @"SELECT c.Id, c.FirstName, c.LastName
+                            FROM Customer c 
+                            WHERE s.Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    Customer customer = null;
+
+                    while (reader.Read())
+                    {
+                        if (customer == null)
+                        {
+                            customer = new Customer
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName"))
+                            };
+                        }
+                    }
+                    reader.Close();
+
+                    return Ok(customer);
+                }
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Customer customer)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Customer (FirstName, LastName, SlackName, CohortId)
+                                        OUTPUT INSERTED.Id
+                                        VALUES (@FirstName, @LastName, @SlackName, @CohortId)";
+                    cmd.Parameters.Add(new SqlParameter("@FirstName", customer._firstname));
+                    cmd.Parameters.Add(new SqlParameter("@LastName", customer._lastname));
+                    cmd.Parameters.Add(new SqlParameter("@SlackName", customer._handle));
+                    cmd.Parameters.Add(new SqlParameter("@CohortId", customer._cohort.Id));
+
+                    int newId = (int)cmd.ExecuteScalar();
+                    customer.Id = newId;
+                    return CreatedAtRoute("GetCustomer", new { id = newId }, customer);
+                }
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Customer customer)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Customer SET FirstName = @FirstName,
+                                            LastName = @LastName, SlackName = @SlackName,
+                                            CohortId = @CohortId WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@FirstName", customer._firstname));
+                        cmd.Parameters.Add(new SqlParameter("@LastName", customer._lastname));
+                        cmd.Parameters.Add(new SqlParameter("@SlackName", customer._handle));
+                        cmd.Parameters.Add(new SqlParameter("@CohortId", customer._cohort.Id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM Customer WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!CustomerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private bool CustomerExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, FirstName, LastName, SlackName, CohortId
+                                        FROM Customer WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    return reader.Read();
+                }
+            }
+        }
+    }
+}
