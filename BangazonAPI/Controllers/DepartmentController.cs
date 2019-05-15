e9781277dafa5de5876f1506defbd8ed0bdf8be6
ANTONIO JEFFERSON <akjeff025@gmail.com>
2019-05-15 10:29:48 -0500

    able to get single department by ID with employees and filter

--- BangazonAPI/Controllers/DepartmentController.cs
+++ BangazonAPI/Controllers/DepartmentController.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
 using BangazonAPI.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace BangazonAPI.Controllers
 {
@@ -34,14 +34,6 @@ namespace BangazonAPI.Controllers
         //this function gets a List of all Customers in the database
         public async Task<IActionResult> Get(string _include, string _filter, string q)
         {
-            //create the SQL as a string, in order to be able to add to it with the 'include' queries
-            //string sql = 
-            //string sql = @"SELECT d.Id, d.[Name], d.Budget, e.Id, e.FirstName, e.LastName, 
-            //              e.DepartmentId, e.IsSupervisor
-            //              FROM Department d
-            //              JOIN Employee e ON d.Id = e.Department.Id
-            //              WHERE 2=2";
-
             string sql_head = @"SELECT d.Id, d.[Name], d.Budget";
             string sql_end = "FROM Department d";
             string sql = $"{sql_head} {sql_end}";
@@ -112,7 +104,7 @@ namespace BangazonAPI.Controllers
                                 });                            
                         };
 
-                        if (_include == "filter")
+                        if (_filter == "300000")
                         {
                             if (!reader.IsDBNull(reader.GetOrdinal("Name")))
                             {
@@ -134,104 +126,113 @@ namespace BangazonAPI.Controllers
                 }
             }
         }
-        //[HttpGet]
-        ////this function gets a List of all Customers in the database
-        //public async Task<IActionResult> GetById(string _include, string _filter, string q)
-        //{
-        //    //create the SQL as a string, in order to be able to add to it with the 'include' queries
+        [HttpGet("{id}", Name = "GetDepartment")]
+        //this function gets a single Customer from the database, by id
+        public async Task<IActionResult> Get([FromRoute] int id, string _include, string _filter, string q)
+        {
+            try
+            {
+                //create the SQL as a string, in order to be able to add to it with the 'include' queries
+                string sql_head = @"SELECT d.Id, d.[Name], d.Budget";
+                string sql_from = @"FROM Department d";
+                string sql_where = @"WHERE d.Id = @id";
+                string sql = $"{sql_head} {sql_from} {sql_where}";
 
-        //    string sql_head = @"SELECT d.Id, d.[Name], d.Budget";
-        //    string sql_end = "FROM Department d";
-        //    string sql = $"{sql_head} {sql_end}";
+                if (_include == "employees") //?_include=employees
+                {
+                    string sql_employee_middle = @", e.Id, e.FirstName, e.LastName, e.DepartmentId, e.IsSupervisor";
+                    string sql_employee_end = @"JOIN Employee e ON d.Id = e.DepartmentId";
+                    sql = $"{sql_head} {sql_employee_middle} {sql_from} {sql_employee_end} {sql_where}";
 
-        //    if (_include == "employees") //?_include=employees
-        //    {
+                    if (q != null)
+                    {
+                        sql = $@"{sql_head} {sql_employee_middle} {sql_from} {sql_employee_end} WHERE 2=2 AND d.Id = @id AND d.Name = '{@q}'";
+                    }
+                }
+                else if (_filter == "300000") //?_filter=300000
+                {
+                    string sql_budget_end = @" WHERE 2=2 AND d.Budget >= 300000 AND d.Id = @id";
+                    sql = $"{sql_head} {sql_from} {sql_budget_end}";
+                    
+                }
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = sql;
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
 
+                        SqlDataReader reader = await cmd.ExecuteReaderAsync();
 
-        //        string sql_employee_middle = @", e.Id, e.FirstName, e.LastName, e.DepartmentId, e.IsSupervisor";
-        //        string sql_employee_end = @"JOIN Employee e ON d.Id = e.DepartmentId";
+                        Department department = null;
 
+                        while (reader.Read())
+                        {
+                            if (department == null)
+                            {
+                                department = new Department
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                    Name = reader.GetString(reader.GetOrdinal("Name")),
+                                    Budget = reader.GetInt32(reader.GetOrdinal("Budget")),
+                                    Employees = new List<Employee>(),
 
-        //        sql = $"{sql_head} {sql_employee_middle} {sql_end} {sql_employee_end}";
+                                };
+                            }
+                            if (_include == "employees")
+                            {
+                              
+                                    department.Employees.Add(new Employee
+                                    {
+                                        Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                        FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                        LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                        DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                                        IsSuperVisor = reader.GetBoolean(reader.GetOrdinal("IsSuperVisor")),
+                                    });
+                                
+                            }
 
-        //        if (q != null)
-        //        {
-        //            sql = $@"{sql_head} {sql_employee_middle} {sql_end} {sql_employee_end} AND d.Name = '{@q}'";
-        //        }
-        //    }
-        //    else if (_filter == "300000") //?_include=budget over 300000
-        //    {
-        //        string sql_budget_end = @"WHERE d.Budget >= 300000";
-        //        sql = $"{sql_head} {sql_end} {sql_budget_end}";
-        //    }
+                            if (_filter == "300000")
+                            {
+                               
+                                department = new Department
+                                    {
+                                        Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                        Name = reader.GetString(reader.GetOrdinal("Name")),
+                                        Budget = reader.GetInt32(reader.GetOrdinal("Budget")),
 
-        //    using (SqlConnection conn = Connection)
-        //    {
-        //        conn.Open();
-        //        using (SqlCommand cmd = conn.CreateCommand())
-        //        {
+                                    };
+                                
+                            }
+                        }
+                        reader.Close();
 
-        //            cmd.CommandText = sql;
-
-        //            if (q != null)
-        //            {
-        //                cmd.Parameters.Add(new SqlParameter("@q", $"%{q}%"));
-
-        //            }
-
-        //            SqlDataReader reader = await cmd.ExecuteReaderAsync();
-        //            List<Department> departments = new List<Department>();
-
-        //            Dictionary<int, Department> DepartmentHash = new Dictionary<int, Department>();
-
-        //            while (reader.Read())
-        //            {
-        //                int departmentId = reader.GetInt32(reader.GetOrdinal("Id"));
-        //                if (!DepartmentHash.ContainsKey(departmentId))
-        //                {
-        //                    DepartmentHash[departmentId] = new Department
-        //                    {
-        //                        Id = departmentId,
-        //                        Name = reader.GetString(reader.GetOrdinal("Name")),
-        //                        Budget = reader.GetInt32(reader.GetOrdinal("Budget")),
-        //                        Employees = new List<Employee>(),
-        //                    };
-        //                };
-
-        //                if (_include == "employees")
-        //                {
-        //                    DepartmentHash[departmentId].Employees.Add(new Employee
-        //                    {
-        //                        Id = reader.GetInt32(reader.GetOrdinal("Id")),
-        //                        FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
-        //                        LastName = reader.GetString(reader.GetOrdinal("LastName")),
-        //                        DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
-        //                        IsSuperVisor = reader.GetBoolean(reader.GetOrdinal("IsSuperVisor")),
-        //                    });
-        //                };
-
-        //                if (_include == "filter")
-        //                {
-        //                    if (!reader.IsDBNull(reader.GetOrdinal("Name")))
-        //                    {
-        //                        DepartmentHash[departmentId] = new Department
-        //                        {
-        //                            Id = departmentId,
-        //                            Name = reader.GetString(reader.GetOrdinal("Name")),
-        //                            Budget = reader.GetInt32(reader.GetOrdinal("Budget")),
-
-        //                        };
-        //                    }
-        //                };
-
-        //                departments = DepartmentHash.Values.ToList();
-        //            }
-        //            reader.Close();
-
-        //            return Ok(departments);
-        //        }
-        //    }
-        //}
+                        if (department == null)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status404NotFound);
+                            throw new Exception("No rows affected");
+                        }
+                        else
+                        {
+                            return Ok(department);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
         [HttpPost]
         //this function adds a single Customer to the database
         public async Task<IActionResult> Post([FromBody] Department department)
